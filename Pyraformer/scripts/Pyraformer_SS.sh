2cda84f85c7ff10c74bf0140e9ee269d476c4be4
Jan <41526754+Jtingo@users.noreply.github.com>
2023-11-30 23:34:02 +0100

    Delete Pyraformer/scripts directory

--- Pyraformer/scripts/Pyraformer_SS.sh
+++
@@ -1,3 +0,0 @@
-python single_step_main.py -data_path data/elect/ -dataset elect;
-python single_step_main.py -data_path data/wind/ -dataset wind;
-python single_step_main.py -data_path data/flow/ -dataset flow;
