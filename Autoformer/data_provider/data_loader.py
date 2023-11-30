684bf066a8c385a45038f5b7311d33060b8fe717
Orri11 <132065094+Orri11@users.noreply.github.com>
2023-11-30 17:03:21 +0100

    Add files via upload

--- Autoformer/data_provider/data_loader.py
+++ Autoformer/data_provider/data_loader.py
@@ -228,12 +228,10 @@ class Dataset_SYNTH_hour(Dataset):
         border1 = border1s[self.set_type]
         border2 = border2s[self.set_type]
         
-        if self.features=='M' or self.features=='MS':
-            #### Added error raise as dataset univariate
-            raise ValueError("M and MS invalid settings. Use univariate setting 'S' for the given dataset.")
-            #cols_data = df_raw.columns[1:]
-            #df_data = df_raw[cols_data]
-        elif self.features=='S':
+        if self.features == 'M' or self.features == 'MS':
+            cols_data = df_raw.columns[1:]
+            df_data = df_raw[cols_data]
+        elif self.features == 'S':
             df_data = df_raw[[self.target]]
 
         if self.scale:
@@ -242,7 +240,7 @@ class Dataset_SYNTH_hour(Dataset):
             data = self.scaler.transform(df_data.values)
         else:
             data = df_data.values
-            
+
         df_stamp = df_raw[['date']][border1:border2]
         df_stamp['date'] = pd.to_datetime(df_stamp.date)
         if self.timeenc == 0:
@@ -258,11 +256,11 @@ class Dataset_SYNTH_hour(Dataset):
         self.data_x = data[border1:border2]
         self.data_y = data[border1:border2]
         self.data_stamp = data_stamp
-    
+
     def __getitem__(self, index):
         s_begin = index
         s_end = s_begin + self.seq_len
-        r_begin = s_end - self.label_len 
+        r_begin = s_end - self.label_len
         r_end = r_begin + self.label_len + self.pred_len
 
         seq_x = self.data_x[s_begin:s_end]
@@ -271,6 +269,12 @@ class Dataset_SYNTH_hour(Dataset):
         seq_y_mark = self.data_stamp[r_begin:r_end]
 
         return seq_x, seq_y, seq_x_mark, seq_y_mark
+
+    def __len__(self):
+        return len(self.data_x) - self.seq_len - self.pred_len + 1
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
     
     def __len__(self):
         return len(self.data_x) - self.seq_len- self.pred_len + 1
@@ -318,12 +322,10 @@ class Dataset_WIND_hour(Dataset):
         border1 = border1s[self.set_type]
         border2 = border2s[self.set_type]
         
-        if self.features=='M' or self.features=='MS':
-            #### Added error raise as dataset univariate
-            raise ValueError("M and MS invalid settings. Use univariate setting 'S' for the given dataset.")
-            #cols_data = df_raw.columns[1:]
-            #df_data = df_raw[cols_data]
-        elif self.features=='S':
+        if self.features == 'M' or self.features == 'MS':
+            cols_data = df_raw.columns[1:]
+            df_data = df_raw[cols_data]
+        elif self.features == 'S':
             df_data = df_raw[[self.target]]
 
         if self.scale:
@@ -332,33 +334,41 @@ class Dataset_WIND_hour(Dataset):
             data = self.scaler.transform(df_data.values)
         else:
             data = df_data.values
-            
+
         df_stamp = df_raw[['date']][border1:border2]
         df_stamp['date'] = pd.to_datetime(df_stamp.date)
-        data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq)
+        if self.timeenc == 0:
+            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
+            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
+            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
+            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
+            data_stamp = df_stamp.drop(['date'], 1).values
+        elif self.timeenc == 1:
+            data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
+            data_stamp = data_stamp.transpose(1, 0)
 
         self.data_x = data[border1:border2]
-        if self.inverse:
-            self.data_y = df_data.values[border1:border2]
-        else:
-            self.data_y = data[border1:border2]
+        self.data_y = data[border1:border2]
         self.data_stamp = data_stamp
-    
+
     def __getitem__(self, index):
         s_begin = index
         s_end = s_begin + self.seq_len
-        r_begin = s_end - self.label_len 
+        r_begin = s_end - self.label_len
         r_end = r_begin + self.label_len + self.pred_len
 
         seq_x = self.data_x[s_begin:s_end]
-        if self.inverse:
-            seq_y = np.concatenate([self.data_x[r_begin:r_begin+self.label_len], self.data_y[r_begin+self.label_len:r_end]], 0)
-        else:
-            seq_y = self.data_y[r_begin:r_end]
+        seq_y = self.data_y[r_begin:r_end]
         seq_x_mark = self.data_stamp[s_begin:s_end]
         seq_y_mark = self.data_stamp[r_begin:r_end]
 
         return seq_x, seq_y, seq_x_mark, seq_y_mark
+
+    def __len__(self):
+        return len(self.data_x) - self.seq_len - self.pred_len + 1
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
     
     def __len__(self):
         return len(self.data_x) - self.seq_len- self.pred_len + 1
