f15045b55a2c4166f459e631c64fca50c1d49cd2
anasashb <anasashb@sunflower.wiwi.hu-berlin.de>
2024-02-01 19:59:38 +0100

    restructured data generator

--- data_generator.py
+++ data_generator.py
@@ -25,23 +25,17 @@ class SynthesisTS:
         self.seq_len = seq_len
 
     # Generates the actual energy time series
-    def _generate_sin(self, time_points, sin_coefficients, trend, trend_slope, trend_rate, reverse_trend, structural_break, break_intensity):
+    def _generate_sin(self, time_points, sin_coefficients):
         '''
         Generates a mixed sinusoidal sequence given the amount of cycle periods, time points, and the amount of coefficients for individual sine functions.            
         '''
         # Empty array corresponding to time points
         y = np.full(len(time_points),100.0)
-        print(f"Generated: {y[0],y[1],y[2]}")
+        
         # Generate individual sine functions to sum up
         for i in range(len(self.cycle_periods)):
             y += sin_coefficients[i] * np.sin(2 * np.pi / self.cycle_periods[i] * time_points)    
-        #if structural_break == True:
-        # Add in a structural break at a random point in time 
-            
-            #break_point = random.randint(1,len(time_points))
-            #for i in range(len(time_points)):
-                #if i > break_point:
-                    #y[i] += break_intensity
+
         return y
         
     # Generates covariates (day of week, hour of day, month of year) for hourly data
@@ -92,18 +86,13 @@ class SynthesisTS:
         noise = np.random.multivariate_normal(mean, cov, (self.series_amount,), 'raise')
         return noise
         
-    def synthesize_single_series(self, trend='None', trend_slope=None, trend_rate=None, reverse_trend=False, structural_break = False, break_intensity = 50):
+    def synthesize_single_series(self):
         '''
         Generates a single time series with a date-time index.
 
-        Args:
-            trend (str): Whether to incorporate a trend in the time series. 'None' for stationary series, 'Additive' for additive trend, and 'Multiplicative' for multiplicative trend.
-            trend_slope (float): (Optional) Slope parameter or additive trend. 0.0005 makes a slight trend for 17,420 data. 0.005 will make a more pronounced trend.
-            trend_rate (float): (Optional) Parameter (e^{trend_rate}) for multiplicative trend.
+        Returns:
+            self.results (pd.DataFrame) - Returs a univariate time series sequence stored in a Pandas DataFrame alongside a time index column.
         '''
-        # Handle error for trend
-        if trend not in ['None', 'Additive', 'Multiplicative']:
-            raise ValueError("Please choose a valid trend parameter from: 'None', 'Additive', 'Multiplicative'.")
         
         # Generate initial time stamp
         init_date_stamp = pd.Timestamp('2022-01-01 00:00:00')
@@ -119,12 +108,13 @@ class SynthesisTS:
         # "Coefficients of the three sine functions B_1, B_2, B_3 for each time series sampled uniformly from [5, 10]"
         sin_coefficients = np.random.uniform(5, 10, 3)
         # Generate time series
-        y = self._generate_sin(time_points, sin_coefficients, trend, trend_slope, trend_rate, reverse_trend, structural_break, break_intensity)
+        y = self._generate_sin(time_points, sin_coefficients)
         # Define mean and covariance of the noise term B_0 - a Gaussian process with a polynomially decaying covariance function.
         mean, cov = self._polynomial_decay_cov()
         # Draw B_0 -s for each time point t for each time series from Gaussian distribution
         noise = self._multivariate_normal(mean, cov) 
         series = y + noise
+        # This specific format is tailored for the temporal embedding used in large majority of Transformer-based TS forecasting frameworks
         df = pd.DataFrame({'date': datetime_index, 'TARGET': series.squeeze()})
 
         # Add as a self variable
@@ -132,10 +122,51 @@ class SynthesisTS:
 
         return self.result
     
+    
     @staticmethod
-    def add_trend(df=None, trend='Additive', trend_slope=0.01, trend_slope_increment=0.005, accumulated_retain_rate=0.5, trend_rate=1.00001, reversal=False, reversal_interval=24*30*3):
+    def add_multiplicative_trend(df=None, trend_rate=1.00005, reversal_offset=0.00002, reversal=False, reversal_interval=24*30*3):
         '''
-        Adds trends and optionally reversals to an existing synthesized time series.
+        Adds a multiplicative trend and optionally reversals to an existing time series.
+        
+        Args:
+            df(pd.DataFrame, optional): DataFrame containing the synthesized time series to apply trend to. Will default to self.result if exists.
+            trend_rate (float): Multiplicative trend rate.
+            reversal_offset (float): A rate to offset the trend rate by to make reversals smoother.
+            reversal (bool): Whether to apply trend reversals.
+        '''
+        if df is None:
+            raise ValueError('No data given: please provide a DataFrame.')
+        
+        df = df.copy()
+        y = df['TARGET'].values + 100 # to shift by 100 remove this later
+        # Define time points t
+        t = np.arange(len(y))
+
+        if reversal == True:
+            if not reversal_interval:
+                reversal_interval = 24*30*3 # quaterly by default
+        
+        reverse_trend_rate = trend_rate - reversal_offset #### 0.00001 and 0.000015 also good options for a bit smoother but less noticable
+        current_rate = trend_rate
+        for i in range(0,len(t)):
+            if reversal and i % reversal_interval == 0 and i != 0:
+                if current_rate == trend_rate:
+                    current_rate = reverse_trend_rate
+                else:
+                    current_rate = trend_rate
+                    # Invert trend rate
+            y[i] = y[i] * ((current_rate ** i))
+                
+
+        result_df = pd.DataFrame({'TARGET': y.squeeze()}, index=df.index)
+        
+        return result_df
+    
+    
+    @staticmethod
+    def add_additive_trend(df=None, trend_slope=0.01, trend_slope_increment=0.005, accumulated_retain_rate=0.5, reversal=False, reversal_interval=24*30*3):
+        '''
+        Adds an additive trend and optionally reversals to an existing time series.
 
         Args:
             df(pd.DataFrame, optional): DataFrame containing the synthesized time series to apply trend to. Will use self.result if existing.
@@ -143,21 +174,16 @@ class SynthesisTS:
             trend_slope (float): Additive trend slope.
             trend_slope_increment (float): Increase trend_rate when coming up from a decreasing trend --- helps maintain overall rising trend.
             accumulated_retain_rate (float): How much of the accumulated trend to retain -- helps make additive trend reversals smoother
-            trend_rate (float): Multiplicative trend rate.
             reversal (bool): Whether to apply trend reversals.
-            reversal_interval (int): How often the reversal occurs - 24*30*3 for quarterly reversals, 24*30*6 for half-a-year.
         '''
         # If dataframe is not given use self.result
         if df is None:
             raise ValueError('No data given: please provide a DataFrame.')
 
         df = df.copy()
-
-        if trend not in ['Additive', 'Multiplicative']:
-            raise ValueError("Invalid trend type. Use either 'Addtive' or 'Multiplicative'.")
         
         # Define series y
-        y = df['TARGET'].values + 100 # to shift by 100
+        y = df['TARGET'].values + 100 # to shift by 100 remove this later
         # Define time points t
         t = np.arange(len(y))
 
@@ -165,46 +191,30 @@ class SynthesisTS:
             if not reversal_interval:
                 reversal_interval = 24*30*3 # quaterly by default
 
-        if trend == 'Additive':
             
-            # Variables to handle trend reversals more smoothly
-            accumulated = 0
-            from_increase_path = True
+        # Variables to handle trend reversals more smoothly
+        accumulated = 0
+        from_increase_path = True
             
-            for i in range(0, len(t)):
-                if reversal and i % reversal_interval == 0 and i != 0:
-                    # Just reverse direction if trend additive
-                    trend_slope = -trend_slope
+        for i in range(0, len(t)):
+            if reversal and i % reversal_interval == 0 and i != 0:
+                # Just reverse direction if trend additive
+                trend_slope = -trend_slope
                     
-                    # Increment and from_increase_path allow us to increment trend slope when reversing from a decrease
-                    # Otherwise it may not catch up
-                    if not from_increase_path:
-                        trend_slope += trend_slope_increment
-                        from_increase_path = True
-                    else:
-                        from_increase_path = False
-                    # Reset some part of cumulation for trend not to catch up too rapidly
-                    accumulated *= accumulated_retain_rate
-                accumulated += trend_slope
+                # Increment and from_increase_path allow us to increment trend slope when reversing from a decrease
+                # Otherwise it may not catch up
+                if not from_increase_path:
+                    trend_slope += trend_slope_increment
+                    from_increase_path = True
+                else:
+                    from_increase_path = False
+                # Reset some part of cumulation for trend not to catch up too rapidly
+                accumulated *= accumulated_retain_rate
+            
+            accumulated += trend_slope
                 
-                y[i] += accumulated
-
-        
-        # Handle trend if given
-        elif trend == 'Multiplicative':
-            ### Reverse trend rate is the key variable that defines how abrupt of a jump the reversals will be
-            reverse_trend_rate = trend_rate - 0.00002 #### 0.00001 and 0.000015 also good options for a bit smoother but less noticable
-            current_rate = trend_rate
-            for i in range(0,len(t)):
-                if reversal and i % reversal_interval == 0 and i != 0:
-                    if current_rate == trend_rate:
-                        current_rate = reverse_trend_rate
-                    else:
-                        current_rate = trend_rate
-                    # Invert trend rate
-                y[i] = y[i] * ((current_rate ** i))
+            y[i] += accumulated
                 
-
         result_df = pd.DataFrame({'TARGET': y.squeeze()}, index=df.index)
         
         return result_df
